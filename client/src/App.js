6bfcf7e31205a7bc79dffdcdd4028261ed4d9e73
jzhong16 <jooleyaz@gmail.com>
2024-04-08 14:10:36 -0700

    aaaaaaaa

--- client/src/App.js
+++ client/src/App.js
@@ -2,19 +2,19 @@ import React, { useState, useEffect } from 'react';
 import Product from './Product';
 import './App.css';
 import Browsing from './Browsing';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import ProductInfo from './ProductInfo';
 import products from './products.json';
 
 function App() {
 
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" exact element={<Browsing database={products}/>}/>
         <Route path="/product/:id" element={<ProductInfo database={products}/>}/>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
